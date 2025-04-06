4580cfb48f7eb1cbebaaeda794963d4fc1fe2cdf
Student408 <theloko.dev@gmail.com>
2025-04-06 19:59:11 +0530

    Fix model names in ASRInterface for consistency

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ class ASRInterface:
     def __init__(self):
         self.available_models = {
             "loko99/whisper_tiny_kannada": "Whisper Tiny KN",
-            "loko99/whisper_small_Kannada": "Whisper Small KN",
-            "loko99/whisper_small_kannada_448": "Whisper Small KN -v2",
+            "loko99/whisper_small_kannada": "Whisper Small KN",
+            "loko99/whisper_small_kannada_v2": "Whisper Small KN V2",
             "loko99/whisper_turbo_kannada": "Whisper Turbo KN",
             "openai/whisper-tiny": "Whisper Tiny",
             "openai/whisper-small": "Whisper Small",
